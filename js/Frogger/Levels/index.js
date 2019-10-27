229caf1b0eea0db43e006fc8900a94c3e3fa0abc
Nate Brooks <nateonguitar@gmail.com>
2019-10-26 23:05:25 -0600

    A little cleanup, ignored our compiled files

--- js/Frogger/Levels/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MainLevel_1 = require("./MainLevel");
-exports.MainLevel = MainLevel_1.MainLevel;
-var MainLevelController_1 = require("./MainLevelController");
-exports.MainLevelController = MainLevelController_1.MainLevelController;
-//# sourceMappingURL=index.js.map
